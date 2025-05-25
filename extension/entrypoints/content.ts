09aab5889c8ae468232ad97bb6a6f226c45cc8ad
Jama Mohamed <jama3137@gmail.com>
2025-05-25 11:36:02 +0300

    Load Vue js content on reddit posts

--- extension/entrypoints/content.ts
+++ extension/entrypoints/content.ts
@@ -1,6 +1,53 @@
+import HelloWorld from '../components/HelloWorld.vue';
+import {ContentScriptContext} from "wxt/utils/content-script-context";
+
+const redditPostUrlPatterns = [
+    'https://www.reddit.com/r/*/comments/*/*',
+    'https://www.reddit.com/r/*/comments/*/comment/*'
+]
+const redditPostUrlPatternMatches = redditPostUrlPatterns.map(pattern => new MatchPattern(pattern));
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
-  },
+    matches: redditPostUrlPatterns,
+    cssInjectionMode: 'ui',
+    async main(ctx) {
+        ctx.addEventListener(window, 'wxt:locationchange', ({newUrl}) => {
+            const hasContainer = document.getElementsByTagName('reddit-tldr-container').length > 0;
+            const isRedditPostUrl = redditPostUrlPatternMatches
+                .some(matcher => matcher.includes(newUrl));
+
+            if (!hasContainer && isRedditPostUrl) {
+                setTimeout(async () => {
+                    await loadContentScript(ctx);
+                }, 1000);
+            }
+        });
+
+        await loadContentScript(ctx);
+    },
 });
+
+async function loadContentScript(ctx: ContentScriptContext) {
+    const ui = await createShadowRootUi(ctx, {
+        name: 'reddit-tldr-container',
+        position: 'inline',
+        anchor: 'shreddit-post',
+        append: 'first',
+        onMount: (redditElement) => {
+            let container = document.createElement("div");
+            container.style.backgroundColor = "white";
+            const app = createApp(HelloWorld);
+            app.mount(container);
+
+            redditElement.prepend(container);
+            return {app, container};
+        },
+        onRemove: (elements) => {
+            elements?.app?.unmount();
+            elements?.container?.remove()
+        },
+    });
+
+    ui.shadowHost.setAttribute("slot", "text-body")
+    ui.mount();
+}
